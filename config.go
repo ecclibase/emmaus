47ce54fe30248b0094e9366954093911f120e14f
Lark Mullins <larkmullins@gmail.com>
2018-11-11 18:56:08 -0500

    config struct for holding configuration

---
+++ config.go
@@ -0,0 +1,19 @@
+package oksana
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+// Config handles a service's configuration
+type Config struct{}
+
+// Load reads and returns a .env file as a map
+func (config *Config) Load() map[string]string {
+	c, err := godotenv.Read()
+	if err != nil {
+		log.Fatal("ERROR: Could not load .env file")
+	}
+	return c
+}
