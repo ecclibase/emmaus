7610171fbae8ab52d6d224ffdceeb3df9fdd2cb9
Lark Mullins <larkmullins@gmail.com>
2018-11-11 18:55:51 -0500

    added required functions

--- response.go
+++ response.go
@@ -9,3 +9,24 @@ type Response struct {
 	Status    int
 	Writer    http.ResponseWriter
 }
+
+// NewResponse creates new Response struct
+func NewResponse(w http.ResponseWriter) (r *Response) {
+	return &Response{Writer: w}
+}
+
+// Write writs the bytes (message) to the client
+func (response *Response) Write(b []byte) (n int, err error) {
+	n, err = response.Writer.Write(b)
+	return
+}
+
+// WriteHeader writes a header to the response writer
+func (response *Response) WriteHeader(code int) {
+	response.Writer.WriteHeader(code)
+}
+
+// Header returns the header information
+func (response *Response) Header() http.Header {
+	return response.Writer.Header()
+}
